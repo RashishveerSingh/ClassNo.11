3ce1a4909e3a47ddb3f5e9c0b227bde98e3acf75
RashishveerSingh <ashstormsaber@gmail.com>
2024-11-01 21:09:54 +0800

    initial

---
+++ main.py
@@ -0,0 +1,7 @@
+# acktivity 1 rait aan gel trai_angle
+
+rowing = int(input("enter de naamber off row u wan:"))
+for meowmeow in range(rowing + 1):
+    for cat in range(meowmeow):
+        print("★ ", end = " ")
+    print()
---
+++ main2.py
@@ -0,0 +1,9 @@
+# acktivity 2 proogrem too deemonseterate loit traiii an gel
+
+row_from_user = int(input("aenteerr de namber u want for rowing"))
+meow_meow = 1
+for cat in range(row_from_user):
+    for kitty in range(cat + 1):
+        print(meow_meow, end = " ")
+        meow_meow += 1
+    print()
